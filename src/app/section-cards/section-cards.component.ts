aa499579ac5331eeb106ebe1f884279a5c8cd036
vvskchaitanya <vvskchaitanya@gmail.com>
2022-09-24 15:53:04 +0000

    work flow updates

--- src/app/section-cards/section-cards.component.ts
+++ src/app/section-cards/section-cards.component.ts
@@ -11,7 +11,7 @@ export class SectionCardsComponent implements OnInit {
   constructor() {
     this.cards.push({image:"assets/icons/company.png",head:"Are you company",content:"",action:"Get Started",route:"/register/company"});
     this.cards.push({image:"assets/icons/consultant.png",head:"Are you consultant",content:"",action:"Get Started",route:"/register/consultant"});
-    this.cards.push({image:"assets/icons/support-img.jpg",head:"Talk to our Team",content:"",action:"Get Started",route:"/contact-us"});
+    this.cards.push({image:"assets/icons/support.png",head:"Talk to our Team",content:"",action:"Get Started",route:"/contact-us"});
    }
 
   ngOnInit(): void {
