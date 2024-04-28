7a42657449892053d7d25f70f23082cf9411f081
vvskchaitanya <vvskchaitanya@gmail.com>
2024-04-28 05:49:57 +0000

    changes

--- src/app/home/section-cards/section-cards.component.ts
+++ src/app/home/section-cards/section-cards.component.ts
@@ -9,9 +9,9 @@ export class SectionCardsComponent implements OnInit {
 
   cards:any[]=[];
   constructor() {
-    this.cards.push({image:"assets/icons/company.png",head:"Are you company",content:"",action:"Get Started",route:"/register/company"});
-    this.cards.push({image:"assets/icons/consultant.png",head:"Are you consultant",content:"",action:"Get Started",route:"/register/consultant"});
-    this.cards.push({image:"assets/icons/support.png",head:"Talk to our Team",content:"",action:"Get Started",route:"/contact-us"});
+    this.cards.push({image:"https://www.shutterstock.com/image-vector/company-icon-vector-isolated-on-600nw-1160051578.jpg",head:"Are you company",content:"",action:"Get Started",route:"/register/company"});
+    this.cards.push({image:"https://static.vecteezy.com/system/resources/thumbnails/005/422/750/small/seo-consultant-icon-free-vector.jpg",head:"Are you consultant",content:"",action:"Get Started",route:"/register/consultant"});
+    this.cards.push({image:"https://www.svgrepo.com/show/421200/business-consultant-consulting.svg",head:"Talk to our Team",content:"",action:"Get Started",route:"/contact-us"});
    }
 
   ngOnInit(): void {
